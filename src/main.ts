290354211e6c8ff0de17abd5c047efe308c6803d
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-03-20 23:21:28 +0000

    Hide secret from being printed in logs

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ async function run(): Promise<void> {
   try {
     const speckleServerUrl: string = core.getInput('speckle_server_url')
     const speckleServerToken: string = core.getInput('speckle_server_token')
+    core.setSecret(speckleServerToken)
     const speckleFunctionPath: string = core.getInput('speckle_function_path')
 
     core.info(`Speckle Server URL: ${speckleServerUrl}`)
-    core.debug(`Speckle Server Token: ${speckleServerToken}`) //FIXME remove this as it's secret
+    core.debug(`Speckle Server Token: ${speckleServerToken}`) //this should be masked in the logs?
     core.info(`Speckle Function Path: ${speckleFunctionPath}`)
 
     core.setOutput('function_id', new Date().toTimeString())
