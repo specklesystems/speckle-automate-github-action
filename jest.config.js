0527ca89d02e329352e8192d95b145cd41a84131
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-03-20 21:54:12 +0000

    WIP: fighting with eslint

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['tests/**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true
+}
