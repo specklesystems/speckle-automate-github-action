06bae081c8682535687876b0145d19d44e9c0b13
Gergő Jedlicska <gergo@jedlicska.com>
2023-08-09 09:29:20 +0200

    remove integration tests

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -6,7 +6,7 @@ describe('Register new version', () => {
     vi.stubEnv('INPUT_SPECKLE_FUNCTION_ID', '{fake}')
     vi.stubEnv('INPUT_SPECKLE_TOKEN', '{token}')
     vi.stubEnv('INPUT_SPECKLE_FUNCTION_COMMAND', 'echo "hello automate"')
-    vi.stubEnv('INPUT_SPECKLE_FUNCTION_INPUT_SCHEMA', '{}')
+    vi.stubEnv('INPUT_SPECKLE_FUNCTION_INPUT_SCHEMA', "{}")
     vi.stubEnv('INPUT_SPECKLE_AUTOMATE_URL', 'http://automate.speckle.internal:3030')
     vi.stubEnv('GITHUB_SHA', 'commitSha')
     vi.stubEnv('GITHUB_REF_TYPE', 'commit')
